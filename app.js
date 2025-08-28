74ebd3d879d427d48a35efbab6d02bd9edb5432d
tejamallam <tejamallam1233@gmail.com>
2025-08-28 16:53:48 +0530

    feat: .
    .
    
    .

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('clickMe');
-    const output = document.getElementById('output');
-    
-    button.addEventListener('click', () => {
-        output.textContent = 'Button was clicked! ' + new Date().toLocaleTimeString();
-    });
-});
